5b66579d680c45f894226db3d0b62fd4bfe3270b
eespinozab <mespinozab@ucenfotec.ac.cr>
2021-10-14 23:45:50 -0600

    layout activity register and list houses

---
+++ app/src/main/java/com/manati/visitor/RegisterHouseActivity.kt
@@ -0,0 +1,11 @@
+package com.manati.visitor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterHouseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_house)
+    }
+}
