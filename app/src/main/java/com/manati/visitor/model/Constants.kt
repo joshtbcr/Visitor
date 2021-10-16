15d1f5c4b6815e015fc35ed99828e6eb7b91cadc
mozuniga93 <mzunigaa@ucenfotec.ac.cr>
2021-10-16 09:27:35 -0600

    constants

---
+++ app/src/main/java/com/manati/visitor/model/Constants.kt
@@ -0,0 +1,10 @@
+package com.manati.visitor.model
+
+/**
+ * Object which is use to store constants throughout the application
+ *
+ * @author Franklin Cardenas
+ */
+object Constants {
+    const val USERTYPE = "userType"
+}
