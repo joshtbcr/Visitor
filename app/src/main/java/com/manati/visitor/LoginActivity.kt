2fceabadc799764720310390873b371f3a6330ff
Joshua Torres <joshtbcr@gmail.com>
2021-10-17 19:35:47 -0600

    Login burned

--- app/src/main/java/com/manati/visitor/LoginActivity.kt
+++ app/src/main/java/com/manati/visitor/LoginActivity.kt
@@ -1,16 +1,17 @@
 package com.manati.visitor
 
+import android.app.AlertDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.drawerlayout.widget.DrawerLayout
 
 
 class LoginActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +23,39 @@ class LoginActivity : AppCompatActivity() {
         val loginLayout = findViewById<ConstraintLayout>(R.id.loginLayout)
         //Makes authentication layout visible at startup.
         loginLayout.visibility = View.VISIBLE
+
+        setupLoginButton()
     }
+
+    private fun setupLoginButton() {
+        val buttonLogIn = findViewById<Button>(R.id.buttonLogIn)
+        buttonLogIn.setOnClickListener {
+            val editTextEmail = findViewById<EditText>(R.id.editTextEmail)
+            if (!editTextEmail.text.isNullOrBlank()) {
+                when(editTextEmail.text.toString()){
+                    "visitante" -> showHome()
+                    "seguridad" -> showHome()
+                    else ->{
+                        val context = this
+                        val alertBuilder = AlertDialog.Builder(context)
+                        alertBuilder.setTitle(title)
+                        alertBuilder.setMessage("Error")
+                        alertBuilder.setPositiveButton("Aceptar", null)
+                        val dialog: AlertDialog = alertBuilder.create()
+                        dialog.show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showHome() {
+        var intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+
+
 
 
 
