6f891007ec740a1fd42321d77432839a13dd1812
mozuniga93 <mzunigaa@ucenfotec.ac.cr>
2021-10-17 22:17:06 -0600

    Security navbar

--- app/src/main/java/com/manati/visitor/model/Constants.kt
+++ app/src/main/java/com/manati/visitor/model/Constants.kt
@@ -1,10 +1,5 @@
 package com.manati.visitor.model
 
-/**
- * Object which is use to store constants throughout the application
- *
- * @author Franklin Cardenas
- */
 object Constants {
     const val USERTYPE = "userType"
 }
